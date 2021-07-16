1c958f70a74ea9a6978b847e6427771afeaff7ad
akkala-karthik <akkalakarthik@gmail.com>
2021-07-16 16:42:41 +0530

    commit at 442

--- SIProject/charitydapp/contracts/CharityContract.sol
+++ SIProject/charitydapp/contracts/CharityContract.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.5.16;
 
 contract CharityChain {
     uint256 a = 10;
-    uint256 b = 20;
+    uint256 b = 200;
     uint256 s;
 
     function getRes() public returns (uint256) {
