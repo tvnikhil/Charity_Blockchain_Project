9266a9d0a112c8ac17b715714c9d0d0289825f70
tej_1404 <kalimeratejakumar@gmail.com>
2021-07-17 11:54:39 +0530

    commit at 11:55

--- SIProject/AngularCharitydapp/src/app/app.module.ts
+++ SIProject/AngularCharitydapp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
