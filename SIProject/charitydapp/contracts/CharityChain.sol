564812b8245a4ced5214b7b4e8b3894b3a41d0ba
tvnikhil <nikhilvenkatat@yahoo.com>
2021-07-19 19:16:14 +0530

    commit at  19:16

---
+++ SIProject/charitydapp/contracts/CharityChain.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.5.16;
+
+contract CharityChain {
+    
+    string public name;
+    
+    constructor() public {
+        name = "Charity Chain";
+    }
+    
+}
