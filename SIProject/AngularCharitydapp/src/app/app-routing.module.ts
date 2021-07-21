830153573956de0b0217b42e2f11a6067a8cefc4
starlord-cyber <venkatj2002@outlook.com>
2021-07-21 22:00:44 +0530

    deleted components

--- SIProject/AngularCharitydapp/src/app/app-routing.module.ts
+++ SIProject/AngularCharitydapp/src/app/app-routing.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-
 const routes: Routes = [
-  {path:"Home",component:HomeComponent},
-  {path:"Login",component:LoginComponent},
-  {path:"Register",component:RegisterComponent},
-  {path:'', redirectTo:'/Home', pathMatch:'full'},
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- SIProject/AngularCharitydapp/src/app/app.module.ts
+++ SIProject/AngularCharitydapp/src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FooterComponent,
-    LoginComponent,
-    HomeComponent,
-    RegisterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
