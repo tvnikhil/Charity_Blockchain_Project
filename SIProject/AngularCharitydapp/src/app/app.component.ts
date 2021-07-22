7287966ac72dd9ec6f19f4fe9e6986e95ccfde78
starlord-cyber <venkatj2002@outlook.com>
2021-07-22 09:42:14 +0530

    ui added

--- SIProject/AngularCharitydapp/src/app/app.component.ts
+++ SIProject/AngularCharitydapp/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'AngularCharitydapp';
 }
